8bb5f245979542f105c475fffd79e2839b8ceb4a
Ayush <ayushmodi2003@gmail.com>
2020-12-21 12:43:55 +0530

    Complete Part 2

--- main03.py
+++ main03.py
@@ -1,12 +1,9 @@
-# 3 steps Right, 1 Down
-'''
-RIGHT = 3
-DOWN = 1
-'''
+###### PART 1 ######
+
 TREE = "#"
-#c = 0
 
 def check(n, length):
+	# Returns the number of time the length has to be subtraced from the next position after DOWN
 	a = 0
 	while n > length:
 		n -= length
@@ -14,17 +11,18 @@ def check(n, length):
 	
 	return a
 
-def trees(RIGHT, DOWN):
+def count_trees(RIGHT, DOWN):
+	global TREE
 	c = 0
-	with open("trial_data.txt", "r") as f:
+
+	with open("data03.txt", "r") as f:
 
 		entries = f.readlines()
-		line = 0
 		t = 1 + RIGHT
 		skip = 0
 
 		for entry in entries:
-			line += 1
+			entry = entry[:-1]  # remove "\n"
 
 			if skip < DOWN:
 				skip += 1
@@ -32,13 +30,15 @@ def trees(RIGHT, DOWN):
 
 			length = len(entry)
 
-			if t > len(entry):
+			if t > length:
 				#print("t = " + str(t))
-				entry = entry[:-1] * (check(t, len(entry[:-1])) + 1)
-				#print(entry)
+				entry = entry * (check(t, len(entry[:-1])) + 1)
+				#print("e -  " + entry)
 			
 			d = entry[:t]
-			#print(d)
+			#print(d + "  " + str(len(d)))
+			#print()
+
 			if d[len(d) - 1] == TREE:
 				c += 1
 
@@ -47,4 +47,16 @@ def trees(RIGHT, DOWN):
 	
 	return c
 
-print("c =" + str(trees(7, 1)))
+print("c =" + str(count_trees(3, 1)))
+
+###### PART 2 ######
+
+directions = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+product = 1
+
+for direction in directions:
+	t = count_trees(direction[0], direction[1])
+	#print(t)
+	product *= t
+
+print(product)
