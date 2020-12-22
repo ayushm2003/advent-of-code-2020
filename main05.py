432d9cd6b3608c9dae239bdfa1cbc64697bf041d
Ayush <ayushmodi2003@gmail.com>
2020-12-22 10:52:36 +0530

    Convert to OOP

--- main05.py
+++ main05.py
@@ -1,13 +1,22 @@
-def new(rows, columns):
+class Boarding:
 	seats = []
-	for i in range(rows):
-		n = []
-		for j in range(columns):
-			n.append(j)
-		seats.append(n)
-	
-	return seats
+	def __init__(self, rows, columns):
+		self.rows = rows
+		self.columns = columns
+
+	def new(self):
+		
+		for i in range(self.rows):
+			n = []
+			for j in range(self.columns):
+				n.append(j)
+			self.seats.append(n)
 
 
-for seat in new(5, 3):
+
+plane = Boarding(5, 3)
+plane.new()
+seats = plane.seats
+
+for seat in seats:
 	print(seat)
