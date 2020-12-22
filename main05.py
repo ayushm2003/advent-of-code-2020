bab1afa87c8de6d37b2150a09ceb0f641a66d877
Ayush <ayushmodi2003@gmail.com>
2020-12-22 15:38:05 +0530

    Add function to delete rows

--- main05.py
+++ main05.py
@@ -5,18 +5,48 @@ class Boarding:
 		self.columns = columns
 
 	def new(self):
-		
+		x = 0
 		for i in range(self.rows):
 			n = []
+			
 			for j in range(self.columns):
-				n.append(j)
+				n.append(x)
+				x += 1
 			self.seats.append(n)
+			#x = 0
+
+	def delete(self, property, direction):
+		row_length = len(self.seats)
+		column_length = len(self.seats[0])
+		if property == "row":
+			mid = int(row_length / 2)
+			if direction == "F":
+				del self.seats[mid:(row_length)]
+				#print(self.seats)
+			elif direction == "B":
+				del self.seats[0:mid]
+				'''
+		elif property == "column":
+			mid = int(column_length / 2)
+			if direction == 
+			'''
 
 
-
-plane = Boarding(5, 3)
+plane = Boarding(128, 8)
 plane.new()
 seats = plane.seats
+print(seats)
 
-for seat in seats:
+plane.delete("row", "B")
+plane.delete("row", "F")
+plane.delete("row", "F")
+plane.delete("row", "F")
+plane.delete("row", "B")
+plane.delete("row", "B")
+plane.delete("row", "F")
+
+print()
+print()
+print()
+for seat in plane.seats:
 	print(seat)
